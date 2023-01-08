d1ca1b01bb75beedad0bbb0799f9a8bc38d24f3a
limsh <sehee167@naver.com>
2023-01-08 15:40:34 +0900

    test: 주문, 할인 도메인 테스트

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.entity.member.Grade;
+import hello.core.entity.member.Member;
+import hello.core.entity.order.Order;
+import hello.core.service.member.MemberService;
+import hello.core.service.member.MemberServiceImpl;
+import hello.core.service.order.OrderService;
+import hello.core.service.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.entity.member.Grade;
+import hello.core.entity.member.Member;
+import hello.core.entity.order.Order;
+import hello.core.service.member.MemberService;
+import hello.core.service.member.MemberServiceImpl;
+import hello.core.service.order.OrderService;
+import hello.core.service.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
