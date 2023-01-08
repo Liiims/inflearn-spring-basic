9c6df22726e1964fb84258138ebb1c3de74be1a3
limsh <sehee167@naver.com>
2023-01-08 16:58:27 +0900

    refactor: AppConfig 에서 역할과 구현을 명확히 구분하기 위한 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.entity.discount.DiscountPolicy;
 import hello.core.entity.discount.FixDiscountPolicy;
+import hello.core.entity.member.MemberRepository;
 import hello.core.entity.member.MemoryMemberRepository;
 import hello.core.service.member.MemberService;
 import hello.core.service.member.MemberServiceImpl;
@@ -10,10 +12,18 @@ import hello.core.service.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 생성자 주입
+        return new MemberServiceImpl(memberRepository()); // 생성자 주입
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
