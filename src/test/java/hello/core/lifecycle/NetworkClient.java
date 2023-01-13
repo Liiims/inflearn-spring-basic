bb580497dbb4635029358bb5ac74e4b985386715
limsh <sehee167@naver.com>
2023-01-14 08:49:52 +0900

    test: 스프링 빈 생명주기 콜백 지원 - InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,16 +1,17 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 /**
  * 외부 네트워크에 미리 연결하는 객체를 하나 생성한다고 가정
  */
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url; // 접속할 서버의 url
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -29,5 +30,24 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    /**
+     * 의존관계 주입이 끝난 후 호출되는 메서드 (초기화 지원)
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("\n=== NetworkClient.afterPropertiesSet ===");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    /**
+     * 빈이 종료될 때 호출되는 메서드 (소멸 지원)
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("\n=== NetworkClient.destroy ===");
+        disconnect();
     }
 }
