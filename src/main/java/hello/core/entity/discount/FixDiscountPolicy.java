e6df12cf31adea42eab121553888aaef7234738f
limsh <sehee167@naver.com>
2023-01-13 18:08:15 +0900

    test: @Autowired 필드명 매칭

--- src/main/java/hello/core/entity/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/entity/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.entity.discount;
 
 import hello.core.entity.member.Grade;
 import hello.core.entity.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인
